c810387713cfcf1a294f75fc08656e2d931092cc
andjan3 <150336624+andjan3@users.noreply.github.com>
2024-09-16 13:35:36 +0200

    added histoiry state and some text in html-files

--- script.js
+++ script.js
@@ -2,22 +2,49 @@ let homeDOM = document.getElementsByTagName('a')[0];
 let aboutDOM = document.getElementsByTagName('a')[1];
 let contactDOM = document.getElementsByTagName('a')[2];
 
+window.addEventListener("popstate", ()=>{
+
+  switch(history.state.page){
+    case 0: 
+    fetchPage('home.html')
+    break
+    case 1: 
+    fetchPage("about.html")
+    break
+    case 2:
+      fetchPage("contact.html")
+      break
+  }
+})
+
 homeDOM.addEventListener('click', ()=>{
   alert('Andréa')
   history.pushState({page:0}, "", "/home")
   fetchPage("home.html")
 })
 
+aboutDOM.addEventListener('click', ()=>{
+  history.pushState({page:1}, "", "/about")
+  fetchPage("about.html")
+})
+
+contactDOM.addEventListener('click', ()=>{
+  history.pushState({page:2}, "", "/contact")
+  fetchPage("contact.html")
+})
+
+
 let contentDOM = document.getElementById('content');
 fetchPage("home.html")
-
+//sätt in progressbar animation
 function fetchPage(filename){
   if(typeof filename != "string") return;
 
   fetch(filename).then((resultat)=>{
-
+    //Avaktivera progressbaren här
 
     return resultat.text()
   }).then( (data)=>contentDOM.innerHTML =data)
   
+  //Lägg in catch error, avaktivera progressbar även här
 }
